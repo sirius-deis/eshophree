f95790540ff10c8b7be83e7eab4fee3a9ae7b124
Sirius Deis <falebsy@gmail.com>
2023-04-30 14:08:00 +0200

    add prop types to dropdown component

--- src/components/dropdown/dropdown.component.jsx
+++ src/components/dropdown/dropdown.component.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
+
 import styles from "./dropdown.styles.module.css";
 
 const Dropdown = ({ children }) => {
     return <div className={styles.dropdown}>{children}</div>;
+};
+
+Dropdown.propTypes = {
+    children: PropTypes.node,
 };
 
 export default Dropdown;
