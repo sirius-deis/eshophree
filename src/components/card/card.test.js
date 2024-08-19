6d1481a152b8452854727a15173f0a79fe9c80b1
Sirius Deis <falebsy@gmail.com>
2024-08-19 21:19:51 +0200

    add tests to a card component for chacking if a product and category links are rendered correctly

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -1,25 +1,40 @@
 import { render, screen } from "@testing-library/react";
 import Card from "./card";
 
+const defaultValues = {
+  _id: "test",
+  name: "Hero",
+  images: ["test.jpg"],
+  categoryId: { _id: "test", name: "Category" },
+  price: 100,
+  total: 10,
+  sold: 5,
+  endIn: 100000,
+};
+
 
 describe("Card component", () => {
   it("should match snapshot", () => {
-    const { container } = render(<Card />);
+    const { container } = render(<Card {...defaultValues} />);
     expect(container).toMatchSnapshot();
   });
 
   it("should render a name", () => {
-    render(<Card name="Test hero" />);
-    expect(screen.getByText("Test hero")).toBeInTheDocument();
+    render(<Card {...defaultValues} />);
+    expect(screen.getByText("Hero")).toBeInTheDocument();
   });
 
-  it("should render a image", () => {
-    render(<Card images={["test.jpg"]} />);
+  it("should render an image", () => {
+    render(<Card {...defaultValues} />);
     expect(screen.getByRole("img")).toHaveAttribute("src", "test.jpg");
   });
 
-  it("should render a description", () => {
-    render(<Card categoryId={{_id: '/id', name: 'some name'}} />);
-    expect(screen.getByText("some name")).toBeInTheDocument();
+  it("should render a product link", () => {
+    render(<Card {...defaultValues} />);
+    expect(screen.getByText("Hero")).toHaveAttribute("href", "test");
+  });
+  it("should render a category link", () => {
+    render(<Card {...defaultValues}  />);
+    expect(screen.getByText("Category")).toHaveAttribute("href", "test");
   });
 });
