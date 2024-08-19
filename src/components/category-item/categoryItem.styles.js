889d4dad6c94f9ceaea46ac7baa32c4d970a3dca
Sirius Deis <falebsy@gmail.com>
2024-08-19 21:21:08 +0200

    move styles to a separate file in a categoryItem component

---
+++ src/components/category-item/categoryItem.styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const StyledItem = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 10px;
+  &:hover {
+    backdrop-filter: brightness(90%);
+  }
+  img {
+    display: flex;
+    padding: 1rem;
+    width: 15rem;
+    height: 15rem;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+  p {
+    font-size: 2rem;
+    text-align: center;
+    &::first-letter {
+      text-transform: uppercase;
+    }
+  }
+`;
