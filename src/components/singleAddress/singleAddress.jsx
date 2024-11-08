72850ef8e874726c8a640743012814aedc38ae7e
Sirius Deis <falebsy@gmail.com>
2024-11-08 19:09:14 +0100

    add state to a singleAddress component

--- src/components/singleAddress/singleAddress.jsx
+++ src/components/singleAddress/singleAddress.jsx
@@ -4,10 +4,18 @@ import Row from "../row/row"
 import { StyledAddress, StyledTitle } from './singleAddress.styles'
 
 const SingleAddress = ({ id, title, name, address, city, state, zipCode, phone, email }) => {
+  const [isBeingEdited, setIsBeingEdited] = useState(false);
+  const toggleEdit = () => {
+    setIsBeingEdited(!isBeingEdited);
+  }
   return <StyledAddress key={id}>
     <Row>
       <StyledTitle>{title}</StyledTitle>
-      <Button onClickHandler={() => setEditId(id)}>Edit</Button>
+      {!isBeingEdited && <Button onClickHandler={toggleEdit}>Edit</Button>}
+      {isBeingEdited && <div>
+        <Button bgColor="primary">Save</Button>
+        <Button bgColor="warning" onClickHandler={toggleEdit}>Cancel</Button>
+      </div>}
     </Row>
     <Line />
     <h3>{name}</h3>
