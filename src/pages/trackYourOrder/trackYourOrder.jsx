a935a681af8ffc8724c9d83a04df1198266f4cb9
Sirius Deis <falebsy@gmail.com>
2024-11-16 23:41:17 +0100

    add a page for tracking order

---
+++ src/pages/trackYourOrder/trackYourOrder.jsx
@@ -0,0 +1,15 @@
+const TrackYourOrder = () => {
+  return <div>
+    <h1>Track Your Order</h1>
+    <p>Please enter your order ID to track your order.</p>
+    <form>
+      <div>
+        <input type="text" placeholder="Order ID" name="id" />
+        <input type="text" placeholder="Email address" name="email" />
+      </div>
+      <button type="submit">Track Order</button>
+    </form>
+  </div>
+}
+
+export default TrackYourOrder;
