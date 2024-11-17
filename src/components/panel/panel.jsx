3c8e6054c529ec8ee32c02e2448d543708f14aa6
Sirius Deis <falebsy@gmail.com>
2024-11-17 23:35:55 +0100

    add bgColor property to a panel component

--- src/components/panel/panel.jsx
+++ src/components/panel/panel.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types'
-import {StyledPanel} from './panel.styles'
+import { StyledPanel } from './panel.styles'
 
-const Panel = ({children, withBorder, withShadow}) => {
-  return <StyledPanel className={`${withBorder ? 'bordered': ''} ${withShadow ? 'shadowed' : ''}`}>{children}</StyledPanel>
+const Panel = ({ children, withBorder, withShadow, bgColor }) => {
+  return <StyledPanel className={`${withBorder ? 'bordered' : ''} ${withShadow ? 'shadowed' : ''}`} bgColor={bgColor}>{children}</StyledPanel>
 }
 
 Panel.propTypes = {
   children: PropTypes.node,
   withBorder: PropTypes.bool,
   withShadow: PropTypes.bool,
+  bgColor: PropTypes.string,
 }
 
 export default Panel;
