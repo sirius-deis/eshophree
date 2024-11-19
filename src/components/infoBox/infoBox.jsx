1aaaa9b4f450e178b1700506ac88fd75260d5517
Sirius Deis <falebsy@gmail.com>
2024-11-19 23:37:32 +0100

    add type property to infoBox component

--- src/components/infoBox/infoBox.jsx
+++ src/components/infoBox/infoBox.jsx
@@ -1,14 +1,20 @@
 import PropTypes from "prop-types";
 import Panel from "../panel/panel";
 
-const InfoBox = ({ error }) => {
-  return <Panel withBorder bgColor="warning">
+const colors = {
+  success: "success",
+  error: "warning",
+}
+
+const InfoBox = ({ error, type }) => {
+  return <Panel withBorder bgColor={colors[type]}>
     <p>{error}</p>
   </Panel>
 }
 
 InfoBox.propTypes = {
   error: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["success", "warning", "error"]),
 }
 
 export default InfoBox;
