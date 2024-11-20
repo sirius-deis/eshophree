4733ad15f51d13de5b05e0bf9bcc911782ca8a35
Sirius Deis <falebsy@gmail.com>
2024-11-20 23:52:06 +0100

    add fetching data to a product page

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -1,5 +1,22 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Spinner from "../../components/spinner/spinner";
+
 const Product = () => {
-    return <div className=''></div>;
+    const { productId } = useParams();
+    const [data, isLoading, error] = useFetch(`/products/${productId}`)
+    if (isLoading) {
+        return <Spinner />
+    }
+    return <div className=''>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <p>Category: <Link>{data.product.category}</Link></p>
+            <h2>{data.product.title}</h2>
+        </div>
+    </div>;
 };
 
 export default Product;
