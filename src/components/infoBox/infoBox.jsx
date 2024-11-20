5a072a1c7d456802154b3a0dbc1669201315b30b
Sirius Deis <falebsy@gmail.com>
2024-11-20 23:37:54 +0100

    add a close button to infoBox component and attach a handler to it

--- src/components/infoBox/infoBox.jsx
+++ src/components/infoBox/infoBox.jsx
@@ -1,20 +1,27 @@
 import PropTypes from "prop-types";
 import Panel from "../panel/panel";
+import Button from "../button/button";
 
 const colors = {
   success: "success",
   error: "warning",
 }
 
-const InfoBox = ({ error, type }) => {
+const InfoBox = ({ error, type, clickHandler }) => {
   return <Panel withBorder bgColor={colors[type]}>
     <p>{error}</p>
+    <div>
+      <Button onClickHandler={clickHandler}>
+        &#10006;
+      </Button>
+    </div>
   </Panel>
 }
 
 InfoBox.propTypes = {
   error: PropTypes.string.isRequired,
   type: PropTypes.oneOf(["success", "warning", "error"]),
+  clickHandler: PropTypes.func,
 }
 
 export default InfoBox;
