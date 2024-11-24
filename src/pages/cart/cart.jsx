b1c17403dd0c83959236dfaf1dc2e9ae6b59f4c9
Sirius Deis <falebsy@gmail.com>
2024-11-24 21:17:04 +0100

    add input for applying coupons to a cart page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,13 @@
+import LabelWithInput from "../../components/label-with-input/labelWithInput";
+import Button from "../../components/button/button";
+
 const Cart = () => {
     return <div className=''>
         <h1>Shopping Cart Summary</h1>
+        <div>
+            <LabelWithInput placeholder="Enter your coupon code" />
+            <Button bgColor="green" color="white">Apply Coupon</Button>
+        </div>
     </div>;
 };
 
