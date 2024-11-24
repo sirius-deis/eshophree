a6cc4d0cb4d183adf4f33626d424b9c3e8989ce3
Sirius Deis <falebsy@gmail.com>
2024-11-24 21:19:35 +0100

    add buttons for firing event to a cart page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,9 +4,14 @@ import Button from "../../components/button/button";
 const Cart = () => {
     return <div className=''>
         <h1>Shopping Cart Summary</h1>
+        <div></div>
         <div>
             <LabelWithInput placeholder="Enter your coupon code" />
             <Button bgColor="green" color="white">Apply Coupon</Button>
+        </div>
+        <div>
+            <Button bgColor="primary" color="white">Continue shopping</Button>
+            <Button bgColor="warning" color="white">Proceed to checkout</Button>
         </div>
     </div>;
 };
