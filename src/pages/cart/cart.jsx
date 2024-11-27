c34cbcba40f700f8fd255d7b15b12c1c39866980
Sirius Deis <falebsy@gmail.com>
2024-11-27 21:13:23 +0100

    add a function for removing a product from the cart by provided id

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,13 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import LabelWithInput from "../../components/label-with-input/labelWithInput";
 import Button from "../../components/button/button";
 import CustomTable from "../../components/customTable/customTable";
+import { removeProductFromCart } from "../../store/cart/cart.actions";
 
 const Cart = () => {
     const { cart, sum } = useSelector(state = state.cart)
+    const dispatch = useDispatch();
+
+    const removeProduct = (productId) => {
+        dispatch(removeProductFromCart(productId))
+    }
     return <div className=''>
         <h1>Shopping Cart Summary</h1>
-        <CustomTable columnsName={["Product", "Price", "Quantity", "Total"]} data={cart} />
+        <CustomTable columnsName={["Product", "Price", "Quantity", "Total"]} data={cart} clickHandler={removeProduct} />
         <div>
             <div>
                 <LabelWithInput placeholder="Enter your coupon code" />
