20dcd7ad756fd06e36e59e71afb512401909d5b3
Sirius Deis <falebsy@gmail.com>
2024-12-06 20:35:48 +0100

    add addressBox to a billingAndShoppingDetails component

--- src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
+++ src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
@@ -1,11 +1,15 @@
+import AddressBox from "../../components/addressBox/addressBox";
+
 const BillingAndShoppingDetails = () => {
   return <div>
     <h1>Billing & Shopping details</h1>
     <h2>
       Billing address
-      <hr />
-
     </h2>
+    <hr />
+    <AddressBox />
+    <h2>Shipping address</h2>
+    <hr />
   </div>
 }
 
