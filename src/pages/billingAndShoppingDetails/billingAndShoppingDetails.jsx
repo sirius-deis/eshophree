3724e44b353016568f4880da5e32a30f6862ea2d
Sirius Deis <falebsy@gmail.com>
2024-12-16 22:11:10 +0100

    add checkboxWithLabel to a billingAndShoppingDetails page

--- src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
+++ src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import AddressBox from "../../components/addressBox/addressBox";
+import CheckboxWithLabel from "../../components/checkboxWithLabel/checkboxWithLabel";
 
 const BillingAndShoppingDetails = () => {
+  const { user } = useSelector(state => state.user);
   return <div>
     <h1>Billing & Shopping details</h1>
     <h2>
@@ -8,6 +11,7 @@ const BillingAndShoppingDetails = () => {
     </h2>
     <hr />
     <AddressBox />
+    {!user && <CheckboxWithLabel labelTitle="Create an account?" />}
     <h2>Shipping address</h2>
     <hr />
   </div>
