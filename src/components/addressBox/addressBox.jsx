18e30fe413830f1ed2b33d15e23a2c46bec2bab2
Sirius Deis <falebsy@gmail.com>
2024-12-19 21:50:27 +0100

    update addressBox component to pass names. add createAccount function to a billingAndShoppingDetails component

--- src/components/addressBox/addressBox.jsx
+++ src/components/addressBox/addressBox.jsx
@@ -3,19 +3,19 @@ import LabelWithInput from "../label-with-input/labelWithInput"
 const AddressBox = () => {
   return <div>
     <div>
-      <LabelWithInput label="First Name" />
-      <LabelWithInput label="Last Name" />
+      <LabelWithInput label="First Name" name="firstName" />
+      <LabelWithInput label="Last Name" name="lastName" />
     </div>
     <div>
-      <LabelWithInput label="Street" />
-      <LabelWithInput label="City" />
-      <LabelWithInput label="State" />
-      <LabelWithInput label="Zip Code" />
-      <LabelWithInput label="Country" />
+      <LabelWithInput label="Street" name="street" />
+      <LabelWithInput label="City" name="city" />
+      <LabelWithInput label="State" name="state" />
+      <LabelWithInput label="Zip Code" name="zipCode" />
+      <LabelWithInput label="Country" name="country" />
     </div>
     <div>
-      <LabelWithInput label="Phone Number" />
-      <LabelWithInput label="Email Address" />
+      <LabelWithInput label="Phone Number" name="phone" />
+      <LabelWithInput label="Email Address" name="email" />
     </div>
   </div>
 }
--- src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
+++ src/pages/billingAndShoppingDetails/billingAndShoppingDetails.jsx
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux";
 import AddressBox from "../../components/addressBox/addressBox";
 import CheckboxWithLabel from "../../components/checkboxWithLabel/checkboxWithLabel";
+import { useState } from "react";
+import fetchData from "../../utils/fetchData";
 
 const BillingAndShoppingDetails = () => {
   const { user } = useSelector(state => state.user);
+  const [shouldAccountBeCreated, setShouldAccountBeCreated] = useState(false)
+  const createAccount = (e) => {
+    const { phone, email } = e.target.elements
+    fetchData("register", { data: { phone, email } })
+  }
   return <div>
     <h1>Billing & Shopping details</h1>
     <h2>
@@ -11,7 +18,7 @@ const BillingAndShoppingDetails = () => {
     </h2>
     <hr />
     <AddressBox />
-    {!user && <CheckboxWithLabel labelTitle="Create an account?" />}
+    {!user && <CheckboxWithLabel labelTitle="Create an account?" clickHandler={setCreateAccount} />}
     <h2>Shipping address</h2>
     <hr />
   </div>
