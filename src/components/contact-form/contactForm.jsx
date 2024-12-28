90eb6204037135f692ab2990f40c1ffccbf1c666
Sirius Deis <falebsy@gmail.com>
2024-12-29 00:55:27 +0100

    update contact form state management

--- src/components/contact-form/contactForm.jsx
+++ src/components/contact-form/contactForm.jsx
@@ -1,25 +1,18 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
 import LabelWithInput from "../label-with-input/labelWithInput";
 import Spinner from "../spinner/spinner";
 import InfoBox from "../infoBox/infoBox";
 import LabelWithTextarea from "../label-with-textarea/labelWithTextarea";
 import fetchData from "../../utils/fetchData";
-import { useState } from "react";
 
 const ContactForm = ({ title, text, clickHandler, isLoading, error, isSubmitted }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [isErrorClosed, setIsErrorClosed] = useState(false);
-  const onSubmitHandler = async (e) => {
+  const onSubmitHandler = (e) => {
     e.preventDefault();
-    try {
-      const { email, subject, message } = e.target.elements
-      setIsErrorClosed(false)
-      setIsLoading(true)
-      await fetchData('contact', { email: email.value, subject: subject.value, message: message.value });
-    } catch (error) {
-      setError(error)
-    }
-    setIsLoading(false)
+    const { email, subject, message } = e.target.elements
+    setIsErrorClosed(false)
+    clickHandler(email.value, subject.value, message.value);
   }
   return <form onSubmit={onSubmitHandler}>
     <h2>{title}</h2>
@@ -27,11 +20,20 @@ const ContactForm = ({ title, text, clickHandler, isLoading, error, isSubmitted 
     <LabelWithInput type="email" label="Email" name="email" />
     <LabelWithInput type="text" label="Subject" name="subject" />
     <LabelWithTextarea label="Your message" name="message" />
-    <Button onClick={clickHandler}>Send message</Button>
+    <Button >Send message</Button>
     {isLoading && <Spinner />}
     {error && !isErrorClosed && <InfoBox message={error} clickHandler={() => setIsErrorClosed(true)} />}
-    {!isLoading && !error && <InfoBox message="Thank you for your submitting!" />}
+    {isSubmitted && !isLoading && !error && <InfoBox message="Thank you for your submitting!" />}
   </form>
+}
+
+ContactForm.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  isSubmitted: PropTypes.bool.isRequired,
 }
 
 export default ContactForm;
