83f4ec5b0259e272df50ea6ae2130bba603160f4
Sirius Deis <falebsy@gmail.com>
2024-12-28 23:48:40 +0100

    add styles to a labelWithTextarea component

---
+++ src/components/label-with-textarea/labelWithTextarea.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLabel = styled.label`
+  font-size: 1.4rem;
+`;
+
+export const StyledTextarea = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  resize: none;
+  `
