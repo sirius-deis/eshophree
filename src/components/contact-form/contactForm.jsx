11bdfac2e139658391ca39639f54bbee4ba5a63c
Sirius Deis <falebsy@gmail.com>
2024-12-29 00:38:52 +0100

    add states to contact form

--- src/components/contact-form/contactForm.jsx
+++ src/components/contact-form/contactForm.jsx
@@ -3,13 +3,22 @@ import Spinner from "../spinner/spinner";
 import InfoBox from "../infoBox/infoBox";
 import LabelWithTextarea from "../label-with-textarea/labelWithTextarea";
 import fetchData from "../../utils/fetchData";
+import { useState } from "react";
 
 const ContactForm = ({ title, text, clickHandler, isLoading, error, isSubmitted }) => {
-
-  const onSubmitHandler = (e) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState(null);
+  const [isErrorClosed, setIsErrorClosed] = useState(false);
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
-    const { email, subject, message } = e.target.elements
-    fetchData('contact', { email: email.value, subject: subject.value, message: message.value });
+    try {
+      const { email, subject, message } = e.target.elements
+      setIsSubmitting(true)
+      await fetchData('contact', { email: email.value, subject: subject.value, message: message.value });
+    } catch (error) {
+
+    }
+    setIsSubmitting(false)
   }
   return <form onSubmit={onSubmitHandler}>
     <h2>{title}</h2>
