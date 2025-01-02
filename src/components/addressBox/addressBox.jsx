dc78419de4769a457c41645852b61889e145391b
Sirius Deis <falebsy@gmail.com>
2025-01-02 21:04:54 +0100

    add rows and columns to addressBox

--- src/components/addressBox/addressBox.jsx
+++ src/components/addressBox/addressBox.jsx
@@ -1,22 +1,31 @@
 import LabelWithInput from "../label-with-input/labelWithInput"
+import Row from "../row/row"
+import Column from "../column/column"
+
 
 const AddressBox = () => {
   return <div>
-    <div>
-      <LabelWithInput label="First Name" name="firstName" />
-      <LabelWithInput label="Last Name" name="lastName" />
-    </div>
-    <div>
-      <LabelWithInput label="Street" name="street" />
-      <LabelWithInput label="City" name="city" />
-      <LabelWithInput label="State" name="state" />
-      <LabelWithInput label="Zip Code" name="zipCode" />
-      <LabelWithInput label="Country" name="country" />
-    </div>
-    <div>
-      <LabelWithInput label="Phone Number" name="phone" />
-      <LabelWithInput label="Email Address" name="email" />
-    </div>
+    <Row>
+      <Column>
+        <LabelWithInput label="First Name" name="firstName" />
+        <LabelWithInput label="Last Name" name="lastName" />
+      </Column>
+    </Row>
+    <Row>
+      <Column>
+        <LabelWithInput label="Street" name="street" />
+        <LabelWithInput label="City" name="city" />
+        <LabelWithInput label="State" name="state" />
+        <LabelWithInput label="Zip Code" name="zipCode" />
+        <LabelWithInput label="Country" name="country" />
+      </Column>
+    </Row>
+    <Row>
+      <Column>
+        <LabelWithInput label="Phone Number" name="phone" />
+        <LabelWithInput label="Email Address" name="email" />
+      </Column>
+    </Row>
   </div>
 }
 
