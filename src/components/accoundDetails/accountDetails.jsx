545368efea1e495ef1eb3e9ad7ac7d66e560f3a7
Sirius Deis <falebsy@gmail.com>
2025-01-03 16:42:21 +0100

    update AccountDetails component to contain state for values provided in the form

--- src/components/accoundDetails/accountDetails.jsx
+++ src/components/accoundDetails/accountDetails.jsx
@@ -3,7 +3,19 @@ import Row from "../row/row";
 import Button from "../button/button";
 
 const AccountDetails = () => {
-  return <div>
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  })
+  const handleChange = e => {
+    setData(prevState => ({ ...prevState, [e.target.name]: e.target.value }))
+  }
+  return <form>
+    <h2>Account Information</h2>
     <Row>
       <LabelWithInput label="First Name" />
       <LabelWithInput label="Last Name" />
@@ -18,7 +30,7 @@ const AccountDetails = () => {
       <LabelWithInput label="Confirm New Password" type="password" />
     </div>
     <Button>Save changes</Button>
-  </div>
+  </form>
 }
 
 export default AccountDetails;
