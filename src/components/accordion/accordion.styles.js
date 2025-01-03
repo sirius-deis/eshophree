74a73703edcff576642f63f36abf2ac4b1d92ab3
Sirius Deis <falebsy@gmail.com>
2025-01-03 16:33:36 +0100

    update styles for accordion component

--- src/components/accordion/accordion.styles.js
+++ src/components/accordion/accordion.styles.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
-export const StyledAccordion = styled.div``;
+export const StyledAccordion = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 10px;
+`;
 
 export const StyledAccordionHeader = styled.div`
   width: 100%;
   display: flex;
+  padding: 10px;
+  cursor: pointer;
 `;
 
 export const StyledAccordionButton = styled.button`
@@ -13,11 +19,16 @@ export const StyledAccordionButton = styled.button`
   display: flex;
   justify-content: space-between;
   font-size: 1.9rem;
-  cursor: pointer;
   border: 0;
   background-color: var(--bg-darker);
 `;
 
-export const StyledAccordionCollapse = styled.div``;
+export const StyledAccordionCollapse = styled.div`
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+`;
 
-export const StyledAccordionBody = styled.div``;
+export const StyledAccordionBody = styled.div`
+  padding: 10px;
+  background-color: #fff;
+`;
